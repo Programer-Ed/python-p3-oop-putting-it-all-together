5fbfb661ba9a662b57a770a7e813cc1826c40e45
Edwin <moyiedwin8@gmail.com>
2024-09-03 09:49:36 +0300

    feat: shoe and book class

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = None
+        self.size = size
+
+    @property
+    def brand(self) -> str:
+        return self._brand
+
+    @brand.setter
+    def brand(self, value: str):
+        if isinstance(value, str):
+            self._brand = value
+        else:
+            print("Brand must be a string")
+
+    @property
+    def size(self) -> int:
+        return self._size
+
+    @size.setter
+    def size(self, value: int):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
